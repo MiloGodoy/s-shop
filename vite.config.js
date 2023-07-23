fe2bf1d7961d7827f2dfe80be3d08c87edb9fcdb
Ramiro Godoy <milogodoy@hotmail.com>
2023-07-23 16:55:04 -0400

    other changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://milogodoy.github.io/",
+  base: "https://milogodoy.github.io/s-shop/",
   plugins: [react()],
   resolve: {
     alias: {
