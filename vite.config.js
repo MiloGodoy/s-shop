1cdb8c0d1b66ace77ca4becc56829b83de166955
Ramiro Godoy <93866055+MiloGodoy@users.noreply.github.com>
2023-07-24 11:41:35 -0400

    Update vite.config.js
    fixing a last bug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://milogodoy.github.io/s-shop/",
+  base: "https://milogodoy.github.io/",
   plugins: [react()],
   resolve: {
     alias: {
